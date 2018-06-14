6252900e7d4e0133b751e3b9b22cdb9b97ed2041
MD MASUM <masum626@hotmail.com>
2018-06-14 16:27:57 +0600

    Initial Project

---
+++ VATReturn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VATReturn.MvcApplication" Language="C#" %>
---
+++ VATReturn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VATReturn.Startup))]
+namespace VATReturn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
